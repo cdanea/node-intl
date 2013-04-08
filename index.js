5fef1faee2f13937081a0a4e16e138e17072698a
Ciprian Danea <ciprian@ro-web-cida>
2013-04-08 13:18:32 +0300

    Added bootstrap files

---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: ciprian
+ * Date: 4/8/13
+ * Time: 12:28 PM
+ * To change this template use File | Settings | File Templates.
+ */
+exports = module.exports = require('./intl.js');
---
+++ intl.js
@@ -0,0 +1,9 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: ciprian
+ * Date: 4/8/13
+ * Time: 12:32 PM
+ * To change this template use File | Settings | File Templates.
+ */
+var i18n = require('i18n');
+
